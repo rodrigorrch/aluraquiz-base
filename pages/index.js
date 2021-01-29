4a900b70d51227998f2980aec3059733cd0f0860
rodrigo-esl <rrodrigo@eslsistemas.com.br>
2021-01-28 22:27:00 -0300

    codigo final da terceira e um pouco da quarta aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
-
-import styled from 'styled-components';
 import db from '../db.json';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
@@ -10,17 +8,7 @@ import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
@@ -44,10 +32,12 @@ export default function Home() {
             }}
             >
               <Input
+                name="nomeDoUsuario"
                 onChange={(infosDoEvento) => {
                   setName(infosDoEvento.target.value);
                 }}
                 placeholder="Diz ai seu nome"
+                value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
                 {`Jogar ${name}`}
